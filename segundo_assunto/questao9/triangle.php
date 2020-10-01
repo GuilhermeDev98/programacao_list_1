806e46f0aad77914ff17e9bca76d32df150e1f81
Guilherme da Silva Santos <guilhermedev@hotmail.com>
2020-10-01 19:33:31 -0300

    questão 10

--- segundo_assunto/questao9/triangle.php
+++ segundo_assunto/questao9/triangle.php
@@ -7,30 +7,15 @@ $sideThree = $_GET['sideThree'];
 $sumOfTwoSides = $sideOne + $sideTwo;
 
 if($sumOfTwoSides > $sideThree){
-    echo "Com as medidas informadas foi encontrado um Triângulo ";
-    if(
-        ($sideOne == $sideTwo && $sideOne == $sideThree) &&
-        ($sideTwo == $sideOne && $sideTwo == $sideThree) &&
-        ($sideThree == $sideOne && $sideThree == $sideTwo)
-    ){
-        echo "Equilátero";
+    echo "Com as medidas informadas, foi encontrado um Triângulo ";
+    if ($sideOne == $sideTwo and $sideOne == $sideThree){
+        echo "Equilátero !";
     }
-
-    if(
-        ($sideOne == $sideTwo && $sideOne == $sideThree) ||
-        ($sideTwo == $sideOne && $sideTwo == $sideThree) ||
-        ($sideThree == $sideOne && $sideThree == $sideTwo)
-    ){
-        echo "Isósceles";
-    }
-
-    if(
-        ($sideOne != $sideTwo && $sideOne != $sideThree) &&
-        ($sideTwo != $sideOne && $sideTwo != $sideThree) &&
-        ($sideThree != $sideOne && $sideThree != $sideTwo)
-    ){
-        echo "Escaleno";
-    }
+    else if ($sideOne == $sideTwo or $sideTwo == $sideThree or $sideThree == $sideOne) {
+        echo "Isósceles !";
+    }else{
+        echo "Escaleno !";           
+    }   
 }else{
     echo "As medidads informadas não formam um triângulo !";
 }
